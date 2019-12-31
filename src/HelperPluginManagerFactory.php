2f25f71e638157fe1c0b0de331f306f1e5a0f9ce
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:45:16 -0600

    Rewriting as Laminas Project package

--- src/HelperPluginManagerFactory.php
+++ src/HelperPluginManagerFactory.php
@@ -1,17 +1,18 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-zendviewrenderer for the canonical source repository
- * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-zendviewrenderer/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-laminasviewrenderer for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-laminasviewrenderer/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-laminasviewrenderer/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace Zend\Expressive\ZendView;
+namespace Mezzio\LaminasView;
 
+use Laminas\ServiceManager\Config;
+use Laminas\View\HelperPluginManager;
 use Psr\Container\ContainerInterface;
-use Zend\ServiceManager\Config;
-use Zend\View\HelperPluginManager;
 
 class HelperPluginManagerFactory
 {
