b874ad9216d46f47d5feca9388964c81c8526b54
George Steel <george@net-glue.co.uk>
2022-09-15 21:18:59 +0100

    Simplify test, fixing psalm issues
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/ExceptionTest.php
+++ test/ExceptionTest.php
@@ -12,7 +12,6 @@ use PHPUnit\Framework\TestCase;
 use function basename;
 use function glob;
 use function is_a;
-use function strrpos;
 use function substr;
 
 class ExceptionTest extends TestCase
@@ -22,15 +21,14 @@ class ExceptionTest extends TestCase
         $this->assertTrue(is_a(ExceptionInterface::class, TemplateExceptionInterface::class, true));
     }
 
+    /** @return Generator<string, array{0: string}> */
     public function exception(): Generator
     {
-        $namespace = substr(ExceptionInterface::class, 0, strrpos(ExceptionInterface::class, '\\') + 1);
-
         $exceptions = glob(__DIR__ . '/../src/Exception/*.php');
         foreach ($exceptions as $exception) {
             $class = substr(basename($exception), 0, -4);
 
-            yield $class => [$namespace . $class];
+            yield $class => ['Mezzio\LaminasView\Exception\\' . $class];
         }
     }
 
