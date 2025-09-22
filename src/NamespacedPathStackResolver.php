42bb4951a53c0f148f8024c1c588fa6b387aea6f
George Steel <george@net-glue.co.uk>
2025-09-22 12:14:48 +0100

    Add retrieval of configured namespaced paths to help implementors of `TemplateRendererInterface`
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/NamespacedPathStackResolver.php
+++ src/NamespacedPathStackResolver.php
@@ -7,8 +7,10 @@ namespace Mezzio\LaminasView;
 use Laminas\View\Exception as ViewException;
 use Laminas\View\Resolver\ResolverInterface;
 use Laminas\View\Resolver\TemplatePathStack;
+use Mezzio\Template\TemplatePath;
 use Override;
 
+use function iterator_to_array;
 use function preg_match;
 
 /**
@@ -64,13 +66,13 @@ final class NamespacedPathStackResolver implements ResolverInterface
     {
         if ($namespace === '') {
             throw new ViewException\InvalidArgumentException(
-                'Invalid namespace provided; must be a non-empty string'
+                'Invalid namespace provided; must be a non-empty string',
             );
         }
 
         if ($path === '') {
             throw new ViewException\InvalidArgumentException(
-                'Invalid path provided; must be a non-empty string'
+                'Invalid path provided; must be a non-empty string',
             );
         }
 
@@ -140,5 +142,27 @@ final class NamespacedPathStackResolver implements ResolverInterface
         $resolver = $this->getNamespace($namespace);
 
         return $resolver->resolve($template);
+    }
+
+    /**
+     * Returns the configured namespaced paths in the correct format for `TemplateRendererInterface`
+     *
+     * This method is internal and may be dropped at any time.
+     *
+     * @return list<TemplatePath>
+     * @psalm-internal Mezzio\LaminasView
+     * @psalm-internal MezzioTest\LaminasView
+     */
+    public function getPathsForMezzioTemplateRenderer(): array
+    {
+        $paths = [];
+        foreach ($this->resolvers as $namespace => $resolver) {
+            $ns = $namespace === self::DEFAULT_NAMESPACE ? null : $namespace;
+            foreach (iterator_to_array($resolver->getPaths(), false) as $path) {
+                $paths[] = new TemplatePath($path, $ns);
+            }
+        }
+
+        return $paths;
     }
 }
